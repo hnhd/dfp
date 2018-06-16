bcaff5359789ef2d68005399052a11d7f60dc936
hnhd <hanna.nizar.haddad@gmail.com>
2018-06-16 19:24:47 -0400

    Add sample data set.

---
+++ R/mc_trump_approval.R
@@ -0,0 +1,14 @@
+#' Morning Consult Trump Approval Rating
+#'
+#' Simple polling data retrieved from the Morning Consult showing
+#' Trump approval ratings across all 50 states for the purpose of
+#' including in ane example.
+#' 
+#' @docType data
+#'
+#' @usage data(mc_trump_approval)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#' @source \href{https://morningconsult.com/tracking-trump/}{QTL Archive}
